c9a24b1dd980868cc83603dddc0acc248494f8b2
Steve Granda <sjgranda@gmail.com>
2018-07-04 17:43:35 -0500

    Initial Working Commit Using BusPirate. This will blink LED on PIN2

---
+++ main.c
@@ -0,0 +1,33 @@
+// main.c
+// A simple blinky program for ATtiny85
+// Connect red LED at pin 2 (PB3)
+
+#include <avr/io.h>
+#include <util/delay.h>
+ 
+ 
+int main (void)
+{
+  // set PB3 to be output
+	DDRB = 0b00001000;
+  while (1) {
+    
+    // flash# 1:
+    // set PB3 high
+    PORTB = 0b00001000; 
+    _delay_ms(20);
+    // set PB3 low
+    PORTB = 0b00000000;
+    _delay_ms(20);
+
+    // flash# 2:
+    // set PB3 high
+    PORTB = 0b00001000; 
+    _delay_ms(200);
+    // set PB3 low
+    PORTB = 0b00000000;
+    _delay_ms(200);
+  }
+ 
+  return 1;
+}
